382d9cb4443219db1b6a513bddb7871b737dbe0a
dududornelees <eduardo.dsdorneles@gmail.com>
2022-10-02 15:26:42 -0300

    ⚙️ Update: Atualizando textos para branco

--- src/components/atoms/CharacterTitle/index.tsx
+++ src/components/atoms/CharacterTitle/index.tsx
@@ -3,8 +3,8 @@ import { Props } from "./types";
 export const CharacterTitle = ({ name, house }: Props) => {
     return (
         <div className="pl-2 pr-4">
-            <h2 className="font-semibold text-lg">{name}</h2>
-            <p className="text-md">{house}</p>
+            <h2 className="font-semibold text-lg text-white">{name}</h2>
+            <p className="text-md text-white">{house}</p>
         </div>
     );
 };
